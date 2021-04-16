14d869b7b01e4df93f1ea5d5a89f5099150d0dbf
Emily <eiz4178@rit.edu>
2021-04-16 14:52:49 -0400

    base enemy creation

---
+++ Assets/Scripts/Enemies/FanArtist.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanArtist : Enemy
+{
+    [SerializeField] private Projectile projectile;
+
+    [SerializeField] protected float fireRate = 0.75f;
+    [SerializeField] protected float fireCooldown = 0.75f;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+
+    protected override void Attack()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= fireRate)
+        {
+            canMove = false;
+            timer = 0;
+            ShootProjectile();
+
+            Debug.Log("Enemy shoots");
+            //if (!InAttackRange())
+            //    canMove = true;
+
+            StopCoroutine(FireCooldown());
+            StartCoroutine(FireCooldown());
+        }
+
+    }
+
+    void ShootProjectile()
+    {
+        Vector2 projectileDirection = target.transform.position - transform.position;
+        Projectile newProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
+
+        // Ignores collision between self and projectile
+        //Physics2D.IgnoreCollision(newProjectile.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+
+        // Ignores collision between all objects in layer
+        newProjectile.Init(gameObject.layer, projectileDirection.normalized);
+
+        //newProjectile.direction = projectileDirection.normalized;
+    }
+
+    // Temp. Will change when animations are in
+    IEnumerator FireCooldown()
+    {
+        yield return new WaitForSeconds(fireCooldown);
+        canMove = true;
+    }
+}
---
+++ Assets/Scripts/Enemies/Projectile.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    //projectile's movement vectors
+    private Vector3 position;
+    public Vector3 direction;
+
+    [SerializeField] private float maxSpeed = 10f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float accelerationRate = 1.05f;
+    [SerializeField] private float damage = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initialize position vector
+        position = transform.position;
+    }
+
+    public void Init(LayerMask layer, Vector2 direction)
+    {
+        Physics2D.IgnoreLayerCollision(layer, gameObject.layer);
+        this.direction = direction;
+        Destroy(gameObject, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // You could just do this with Destroy(this, float time)
+        //StartCoroutine(KillProjectile());
+
+        //move towards target
+        position += direction * speed * Time.deltaTime;
+        transform.position = position;
+
+        //increase speed
+        if (speed <= maxSpeed)
+        {
+            speed *= accelerationRate;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //IDamagable damagable = collision.gameObject.GetComponent<IDamagable>();
+
+        //if (damagable != null)
+            //damagable.DamageAndKnockback(damage, transform.position);
+
+        Destroy(gameObject);
+    }
+
+    private IEnumerator KillProjectile()
+    {
+        //destroy the bullet if it doesn't collide with something for 3 seconds
+        yield return new WaitForSecondsRealtime(1);
+        Destroy(this.gameObject);
+    }
+}
