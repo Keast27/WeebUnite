3fca485aa113bf2989d42eb5a81f5b92f3dab67e
Kayleigh <keast1027@gmail.com>
2021-04-18 12:37:09 -0400

    Player controller made

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float speed;
+    private float velocity;
+    public int health;
+    Vector2 movement;
+
+    enum weapons
+    {
+        foamSword,
+        wand
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    
+    }
+
+    private void FixedUpdate()
+    {
+        move();
+    }
+
+    void move()
+    {
+        velocity = speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement * velocity);
     }
 }
