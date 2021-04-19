e452e817017ef6a1b8645532e53309ae80eefff4
Destiny Treptau <treptaudestiny@gmail.com>
2021-04-19 13:12:34 -0500

    Emily Told Me to Do This
    So I Did

---
+++ Assets/Scripts/RoomScripts/EnemySpawn.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public int enemyCap;
+    public int difficulty;
+
+    public GameObject catGirl;
+    public GameObject fanArtist;
+}
