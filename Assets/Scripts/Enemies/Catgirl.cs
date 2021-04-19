2affbe42dab7dfb80319258f5accb513fbfbcfea
Emily <eiz4178@rit.edu>
2021-04-19 15:28:22 -0400

    work on catgirl
    meow <3

--- Assets/Scripts/Enemies/Catgirl.cs
+++ Assets/Scripts/Enemies/Catgirl.cs
@@ -5,14 +5,48 @@ using UnityEngine;
 public class Catgirl : Enemy
 {
     // Start is called before the first frame update
+
+    [SerializeField] private float pounceTime;
+    private bool pounceFinish;
+    public float pounceTimer;
+    private Vector2 pouncePos;
+
     void Start()
     {
         base.Start();
+        pounceFinish = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         base.Update();
+    }
+
+    protected override void AggroMovement()
+    {
+        if (pounceFinish)
+        {
+            pounceTimer += Time.deltaTime;
+
+            // Jump every second
+            if (pounceTimer > 1)
+            {
+                // Get a random number and then jump anywhere from 50 to 75 % to the player
+                float ran_jump = Random.Range(.5f, 1.0f);
+                pouncePos = ran_jump * (new Vector3(target.transform.position.x, target.transform.position.y + 0.35f, target.transform.position.z) - transform.position) + transform.position;
+                // Hoping is finished, set timer to 0
+                pounceFinish = false;
+                //animator.SetBool("hopfinish", false);
+                pounceTimer = 0;
+
+                // Activate the tween once which will restart the jumping at the end
+                LeanTween.move(gameObject, pouncePos, pounceTime).setEase(LeanTweenType.easeInOutCubic).setOnComplete(() =>
+                {
+                    pounceFinish = true;
+                    //animator.SetBool("hopfinish", true);
+                });
+            }
+        }
     }
 }
