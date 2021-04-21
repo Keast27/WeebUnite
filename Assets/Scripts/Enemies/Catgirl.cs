e89dacf204b00960a561a86037d3a6ff527d98da
Emily <eiz4178@rit.edu>
2021-04-21 13:39:31 -0400

    Catgirl no longer teleports
    nya~

--- Assets/Scripts/Enemies/Catgirl.cs
+++ Assets/Scripts/Enemies/Catgirl.cs
@@ -15,38 +15,55 @@ public class Catgirl : Enemy
     {
         base.Start();
         pounceFinish = true;
+        pounceTime = 0.75f;
     }
-
     // Update is called once per frame
     void Update()
     {
         base.Update();
+
+        /*
+        pounceTimer += Time.deltaTime;
+
+        if(pounceTimer > 5)
+        {
+            moveSpeed = 0;
+            Attack();
+            if(pounceTimer > 6.5)
+            {
+                moveSpeed = 5;
+                pounceTimer = 0;
+            }
+        }
+        */
     }
 
-    //protected override void AggroMovement()
-    //{
-    //    if (pounceFinish)
-    //    {
-    //        pounceTimer += Time.deltaTime;
-    //
-    //        // Jump every second
-    //        if (pounceTimer > 1)
-    //        {
-    //            // Get a random number and then jump anywhere from 50 to 75 % to the player
-    //            float ran_jump = Random.Range(.5f, 1.0f);
-    //            pouncePos = ran_jump * (new Vector3(target.transform.position.x, target.transform.position.y + 0.35f, target.transform.position.z) - transform.position) + transform.position;
-    //            // Hoping is finished, set timer to 0
-    //            pounceFinish = false;
-    //            //animator.SetBool("hopfinish", false);
-    //            pounceTimer = 0;
-    //
-    //            // Activate the tween once which will restart the jumping at the end
-    //            LeanTween.move(gameObject, pouncePos, pounceTime).setEase(LeanTweenType.easeInOutCubic).setOnComplete(() =>
-    //            {
-    //                pounceFinish = true;
-    //                //animator.SetBool("hopfinish", true);
-    //            });
-    //        }
-    //    }
-    //}
+    
+    protected override void AggroMovement()
+    {
+        if (pounceFinish)
+        {
+            pounceTimer += Time.deltaTime;
+
+            // Jump every second
+            if (pounceTimer > 1)
+            {
+                // Get a random number and then jump anywhere from 50 to 75 % to the player
+                float ran_jump = Random.Range(.5f, 1.0f);
+                pouncePos = ran_jump * (new Vector3(target.transform.position.x, target.transform.position.y + 0.35f, target.transform.position.z) - transform.position) + transform.position;
+                // Hoping is finished, set timer to 0
+                pounceFinish = false;
+                //animator.SetBool("hopfinish", false);
+                pounceTimer = 0;
+
+                // Activate the tween once which will restart the jumping at the end
+                LeanTween.move(gameObject, pouncePos, pounceTime).setEase(LeanTweenType.easeInOutCubic).setOnComplete(() =>
+                {
+                    pounceFinish = true;
+                    //animator.SetBool("hopfinish", true);
+                });
+            }
+        }
+    }
+    
 }
