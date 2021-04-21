62670166691e9f9fd1d32c9ad2d1b6453c7c4882
Kayleigh <keast1027@gmail.com>
2021-04-21 19:24:38 -0400

    Attack and charge architecture set up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private GameObject player;
-    private Rigidbody2D rb;
-    public float speed;
-    public float velocity;
-    public int health;
-    Vector2 movement;
 
-    enum weapons
+    public enum weapons
     {
         foamSword,
         wand
     }
+
+    private GameObject player;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRend;
+    public float speed;
+    public float velocity;
+    public int health;
+    Vector2 movement;
+    private weapons selectedWeapon = weapons.foamSword;
+
+    public GameObject projectile;
+    public float fireDelta = 0.5F;
+
+    public float nextFire = 3.5F;
+    private GameObject newProjectile;
+    public float myTime = 0.0F;
+    [SerializeField]  private bool charged = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        spriteRend = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -26,14 +39,79 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-    
+
+       
+
+        if (Input.GetButtonDown("Fire3") || Input.GetButton("Fire1"))
+        {
+            myTime = myTime + Time.deltaTime;
+
+            if (myTime > nextFire && !charged)
+            {
+                nextFire = myTime + fireDelta;
+                Debug.Log("CHARGED");
+                charged = true;
+            }
+        }
+        spriteRend.color = Color.white;
+
+        if (Input.GetButtonUp("Fire3") || Input.GetButtonUp("Fire1"))
+        {
+
+            Debug.Log("Hit!!");
+            nextFire = 3.5f;
+            myTime = 0.0F;
+            weaponMoves();
+            charged = false;
+        }
+
     }
 
     private void FixedUpdate()
     {
         move();
-    }
+        /*
+        if (Input.GetButtonUp("Fire3") || Input.GetButtonUp("Fire1"))
+        {
+            Debug.Log("I hope I'm good at programming");
+            spriteRend.color = Color.cyan;
+            weaponMoves();
+        }
+        */
 
+        
+        }
+
+    private void weaponMoves()
+    {
+        switch (selectedWeapon)
+        {
+            case weapons.foamSword:
+                if (charged)
+                {
+                    Debug.Log("Charged Sword Bonk");
+                    
+                }
+                else
+                {
+                    Debug.Log("Normal Sword Bop");
+                }
+
+                break;
+            case weapons.wand:
+                if (charged)
+                {
+                    Debug.Log("PEWPEWPEW");
+                    
+                }
+                else
+                {
+                    Debug.Log("pew");
+                }
+                break;
+        }
+        
+    }
     void move()
     {
         velocity = speed * Time.fixedDeltaTime;
