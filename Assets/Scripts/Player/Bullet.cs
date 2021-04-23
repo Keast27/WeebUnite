3302df6c8cc059d5b57079c1d0e7d9ad675d9c07
Kayleigh <keast1027@gmail.com>
2021-04-23 18:24:20 -0400

    Fixed bullet, disappears now

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //   public int dir = 1;
+    //  private Rigidbody2D rb;
+    //   float bulletSpeed = 10;
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.velocity = new Vector2(dir * bulletSpeed , 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Door")
+        {
+            Debug.Log("Shot " + collision.name);
+            Destroy(gameObject);
+        }
+    }
+}
